85d394492bea25e856e9499f5000538d7680d5ba
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-10-21 18:47:53 -0300

    inicia rota login

---
+++ app/backend/src/controllers/loginController.ts
@@ -0,0 +1,15 @@
+// import { Request, Response } from 'express';
+// import IloginService from 'src/entities';
+
+// export default class loginController {
+//   private loginService: IloginService;
+
+//   constructor(loginService: IloginService) {
+//     this.loginService = loginService;
+//   }
+
+//   public makeLogin = async (req: Request, res: Response) => {
+//     const { email, password } = req.body;
+//     return res.sendStatus(200);
+//   };
+// }
---
+++ app/backend/src/routes/loginRoutes.ts
@@ -0,0 +1,8 @@
+// import { Router } from 'express';
+// import loginController from 'src/controllers/loginController';
+
+// const userRouter = Router();
+
+// userRouter.post('/login', loginController);
+
+// export default { userRouter };
