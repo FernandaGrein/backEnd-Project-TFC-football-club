eb7183deb74b4c0362aee2ec8d3d8523ea79c7b8
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-10-27 17:13:06 -0300

    adiciona testes da rota leaderboard

---
+++ app/backend/src/tests/leaderboars.test.ts
@@ -0,0 +1,66 @@
+import * as sinon from "sinon";
+import * as chai from "chai";
+// @ts-ignore
+import chaiHttp = require("chai-http");
+
+import { app } from "../app";
+
+import sequelize from "../database/models";
+import {  mockAwayBoard, mockgeneralBoard, mockHomeBoard } from "./mocks";
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe("Testa da rota /leaderboard", () => {
+  describe("testa se é possível buscar os quadros de pontos com sucesso", () => {
+    before(async () => {
+      sinon.stub(sequelize, 'query')
+      .onFirstCall().resolves([mockHomeBoard] as any)
+      .onSecondCall().resolves([mockAwayBoard] as any)
+      .onThirdCall().resolves([mockgeneralBoard] as any)
+    });
+
+    after(() => sinon.restore());
+
+    it("testa se é buscar o placar dos times da casa com sucesso", async () => {
+      const response = await chai.request(app).get("/leaderboard/home");
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal(mockHomeBoard);
+    });
+
+    it("testa se é buscar o placar dos times de fora com sucesso", async () => {
+      const response = await chai
+        .request(app)
+        .get("/leaderboard/away")
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal(mockAwayBoard);
+    });
+
+    it("testa se é buscar o placar geral de todos os times com sucesso", async () => {
+        const response = await chai
+          .request(app)
+          .get("/leaderboard")
+  
+        expect(response.status).to.be.equal(200);
+        expect(response.body).to.be.deep.equal(mockgeneralBoard);
+    });
+
+
+  });
+
+//   describe("testa que não é possível atualizar um jogo inexistente", () => {
+//     before(async () => {
+//       sinon.stub(Model, "update").resolves();
+//       sinon.stub(Model, "findOne").resolves(null as null);
+//     });
+
+//     after(() => sinon.restore());
+//     it("testa  que não é possível finalizar um jogo inexistente", async () => {
+//       const response = await chai.request(app).patch("/matches/99999/finish");
+//       expect(response.status).to.be.equal(404);
+//       expect(response.body).to.be.deep.equal({ message: "Match not found" });
+//     });
+//   });
+});
